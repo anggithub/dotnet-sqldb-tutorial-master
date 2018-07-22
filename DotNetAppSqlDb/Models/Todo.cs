e56215645c794d424c39d49c819d18809bb3d2a9
Angus Gurung <angus.gurung@outlook.com>
2018-07-22 21:34:51 +0800

    Added 'Done' checkbox

--- DotNetAppSqlDb/Models/Todo.cs
+++ DotNetAppSqlDb/Models/Todo.cs
@@ -8,6 +8,8 @@ namespace DotNetAppSqlDb.Models
         public int ID { get; set; }
         public string Description { get; set; }
 
+        public bool Done { get; set; } //angus added for test
+
         [Display(Name = "Created Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
